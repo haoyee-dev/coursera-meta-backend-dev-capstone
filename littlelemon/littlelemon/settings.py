714ac3a60e700d75503eb9231c2136127787bb1f
Hao Yee Chan <haoyee.chan@haoyee.me>
2025-05-25 18:03:08 +1000

    Added Authentication (module 3)

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'restaurant',
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
+    'restaurant',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +131,20 @@ STATIC_URL = 'restaurant.static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# DJOSER
+DJOSER = {"USER_ID_FIELD":"username"}
+
+
+# Django REST Framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        # You can add other authentication classes here if needed
+    ],
+    # You can add other DRF settings here, like DEFAULT_PERMISSION_CLASSES
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    # 'rest_framework.permissions.IsAuthenticated',
+    # ]
+}
